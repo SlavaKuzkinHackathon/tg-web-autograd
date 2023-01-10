a6621319ed6406f1d2ea2bff51c21973032f43c7
SlavaKuzkinHackathon <jess.888@yandex.ru>
2023-01-10 15:43:53 +0700

    commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import { useEffect } from "react";
+
+
+const tg = window.Telegram.WebApp;
+
+
+function App() {
+
+  useEffect(() => {
+    tg.ready()
+  }, [])
+
+  const onClose = () => {
+    tg.close()
+  }
+
+
+  return (
+    <div className="App">
+      Work too
+      <button onClick={onClose}>Закрыть</button>
+    </div>
+  );
+}
+
+export default App;
