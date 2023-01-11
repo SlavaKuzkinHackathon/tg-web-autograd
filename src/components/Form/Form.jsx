cf8e990bce328b6b3c39a57bbd3e74ca08563248
SlavaKuzkinHackathon <jess.888@yandex.ru>
2023-01-11 10:00:27 +0700

    four

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,13 +1,78 @@
-import React from "react";
-import './Form.css'
+import React, { useCallback, useEffect, useState } from 'react';
+import './Form.css';
+import { useTelegram } from "../../hooks/useTelegram";
 
 const Form = () => {
-    return(
-        <div>
-            Form
+    const [carBrand, setСarBrand] = useState('');
+    const [vin, setVin] = useState('');
+    const [subject, setSubject] = useState('physical');
+    const { tg } = useTelegram();
+
+    const onSendData = useCallback(() => {
+        const data = {
+            carBrand,
+            vin,
+            subject
+        }
+        tg.sendData(JSON.stringify(data));
+    }, [carBrand, vin, subject])
+
+    useEffect(() => {
+        tg.onEvent('mainButtonClicked', onSendData)
+        return () => {
+            tg.offEvent('mainButtonClicked', onSendData)
+        }
+    }, [onSendData])
+
+    useEffect(() => {
+        tg.MainButton.setParams({
+            text: 'Отправить данные'
+        })
+    }, [])
+
+    useEffect(() => {
+        if (!vin || !carBrand) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+        }
+    }, [carBrand, vin])
+
+    const onChangeCarBrand = (e) => {
+        setСarBrand(e.target.value)
+    }
+
+    const onChangeVin = (e) => {
+        setVin(e.target.value)
+    }
+
+    const onChangeSubject = (e) => {
+        setSubject(e.target.value)
+    }
+
+    return (
+        <div className={"form"}>
+            <h3>Введите данные для поиска автозапчастей</h3>
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Введите наименование авто'}
+                value={carBrand}
+                onChange={onChangeCarBrand}
+            />
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Введите VIN или номер кузова авто'}
+                value={vin}
+                onChange={onChangeVin}
+            />
+            <select value={subject} onChange={onChangeSubject} className={'select'}>
+                <option value={'physical'}>Моторное масло</option>
+                <option value={'legal'}>Передний амортизатор</option>
+            </select>
         </div>
-    )
-}
+    );
+};
 
-
-export default Form
+export default Form;
