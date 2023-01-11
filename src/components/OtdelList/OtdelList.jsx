0fabba2c2a730bad22b28d280051aed91587cac2
SlavaKuzkinHackathon <jess.888@yandex.ru>
2023-01-11 15:32:47 +0700

    six_7

--- src/components/OtdelList/OtdelList.jsx
+++ src/components/OtdelList/OtdelList.jsx
@@ -5,7 +5,7 @@ import { useTelegram } from "../../hooks/useTelegram";
 import { useCallback, useEffect } from "react";
 
 const products = [
-    { id: '1', title: 'Корея-Маркет', phone: 79139029363 , description: 'Автозапчасти для корейских авто', telegram: '@koreamarket_nsk'},
+    { id: '1', title: 'Корея-Маркет', phone: 79139029363, description: 'Автозапчасти для корейских авто', telegram: '@koreamarket_nsk' },
     { id: '2', title: 'АвтоЕвропа', phone: 79137005382, description: 'Все запчасти для европейских брендов' },
     { id: '3', title: 'Скиф', phone: 79833081822, description: 'Автозапчасти, аккумуляторы, автохимия' },
     { id: '4', title: 'АвтоградШина', phone: 79137009150, description: 'Шины, диски' },
@@ -16,8 +16,39 @@ const products = [
 
 const OtdelList = () => {
     const [addedItems, setAddedItems] = useState([])
+    const { tg, queryId } = useTelegram();
+
+    const onSendData = useCallback(() => {
+        const data = {
+            products: addedItems,
+            queryId,
+        }
+        fetch('http://localhost:3000', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data)
+        })
+    }, [addedItems])
+
+    useEffect(() => {
+        tg.onEvent('mainButtonClicked', onSendData)
+        return () => {
+            tg.offEvent('mainButtonClicked', onSendData)
+        }
+    }, [onSendData])
 
     const onAdd = () => {
+        const alreadyAdded = addedItems.find(item => item.id === product.id);
+        let newItems = [];
+
+        if (alreadyAdded) {
+            newItems = addedItems.filter(item => item.id !== product.id);
+        } else {
+            newItems = [...addedItems, product];
+        }
+
         setAddedItems(newItems)
     }
 
