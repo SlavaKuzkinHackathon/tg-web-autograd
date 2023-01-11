655288cd7c055b6aa831b939567effbf90745780
SlavaKuzkinHackathon <jess.888@yandex.ru>
2023-01-11 14:34:06 +0700

    six

--- src/components/OtdelList/OtdelList.jsx
+++ src/components/OtdelList/OtdelList.jsx
@@ -15,49 +15,10 @@ const products = [
 
 
 const OtdelList = () => {
-    const [addedItems, setAddedItems] = useState([]);
-    const { tg, queryId } = useTelegram();
+    const [addedItems, setAddedItems] = useState([])
 
-    const onSendData = useCallback(() => {
-      /*   const data = {
-            products: addedItems,
-            queryId,
-        } */
-        fetch('http://localhost:3000', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data)
-        })
-    }, [addedItems])
-
-   /*  useEffect(() => {
-        tg.onEvent('mainButtonClicked', onSendData)
-        return () => {
-            tg.offEvent('mainButtonClicked', onSendData)
-        }
-    }, [onSendData]) */
-
-    const onAdd = (product) => {
-        /* const alreadyAdded = addedItems.find(item => item.id === product.id);
-        let newItems = [];
-
-        if (alreadyAdded) {
-            newItems = addedItems.filter(item => item.id !== product.id);
-        } else {
-            newItems = [...addedItems, product];
-        } */
-
+    const onAdd = () => {
         setAddedItems(newItems)
-
-        /* if (newItems.length === 0) {
-            tg.MainButton.hide();
-        } else {
-            tg.MainButton.show();
-            tg.MainButton.setParams({
-            })
-        } */
     }
 
     return (
