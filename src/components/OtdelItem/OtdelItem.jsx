2f3412239ba29d8d44a6c07326122bcfdffe7d6d
SlavaKuzkinHackathon <jess.888@yandex.ru>
2023-01-12 18:41:00 +0700

    12.04_99

--- src/components/OtdelItem/OtdelItem.jsx
+++ src/components/OtdelItem/OtdelItem.jsx
@@ -15,7 +15,8 @@ const OtdelItem = ({ product, className, onAdd }) => {
             <div className={'img'} />
             <div className={'title'}>{product.title}</div>
             <div className={'description'}>{product.description}</div>
-            <a href="tg://resolve?domain=koreamarket_nsk" target="_blank">{product.telegram}</a>
+            <a href="tg://resolve?domain=koreamarket_nsk" target="_blank">@{product.telegram}</a>
+            <a href="https://t.me/koreamarket_nsk" target="_blank">@{product.telegram}</a>
             <Button className={'add-btn'} onClick={onAddHandler}>
                 позвонить {`+${product.phone}`}
             </Button>
