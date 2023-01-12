a77012a5654fc5602d2ba4bd1406bd9d834e154a
SlavaKuzkinHackathon <jess.888@yandex.ru>
2023-01-12 11:41:54 +0700

    111

--- src/components/OtdelItem/OtdelItem.jsx
+++ src/components/OtdelItem/OtdelItem.jsx
@@ -14,11 +14,11 @@ const OtdelItem = ({ product, className, onAdd }) => {
             <div className={'title'}>{product.title}</div>
             <div className={'description'}>{product.description}</div>
             <div className={'telegram'}>{product.telegram}</div>
-            <a className={'telega'} href="https://t.me/">@{product.telegram}</a>
+            <a className={'telega'} href="https://t.me/`${product.telegram}`"> {product.telegram}</a>
             <Button className={'add-btn'} onClick={onAddHandler}>
                 позвонить {`+${product.phone}`}
             </Button>
-
+            
         </div>
     );
 };
