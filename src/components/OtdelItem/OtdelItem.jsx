194ddf77fbe523f9bddcefb0f6fd07461f32e3ec
SlavaKuzkinHackathon <jess.888@yandex.ru>
2023-01-13 16:06:20 +0700

    123_3

--- src/components/OtdelItem/OtdelItem.jsx
+++ src/components/OtdelItem/OtdelItem.jsx
@@ -17,16 +17,22 @@ const OtdelItem = ({ product, className, onAdd }) => {
             <div className={'description'}>{product.description}</div>
             <a className={'telega'} href={`https://t.me/${product.telegram}`} target="_blank">{product.telegram}</a>
             <Button className={'add-btn'} onClick={onAddHandler} href={`tel:+${product.phone}`}>
-                позвонить {`+${product.phone}`}
+                позвонить +${product.phone_number}
             </Button>
-            <a className={'phone'} href={`tg://tel:${product.phone}`} target="_blank">
-                 {`${product.phone}`}
+            <a className={'phone'} href={`tel:${product.phone_number}`} target="_blank">
+                {product.phone_number}
             </a>
-            <a className={'phone'} href={`https://tel:${product.phone}`} target="_blank">
-                 {`${product.phone}`}
+            <a className={'phone'} href={`tg://tel:${product.phone_number}`} target="_blank">
+                {product.phone_number}
             </a>
-            <a className={'phone'} href={`tg://+${product.phone}`} >
-                 {`${product.phone}`}
+            <a className={'phone'} href={`https://tel:${product.phone_number}`} target="_blank">
+                {product.phone_number}
+            </a>
+            <a className={'phone'} href={`tg://+${product.phone_number}`} >
+                {product.phone_number}
+            </a>
+            <a className={'phone'} href={`tg://+${product.phone_number}`} >
+                {product.phone_number}
             </a>
         </div>
     );
