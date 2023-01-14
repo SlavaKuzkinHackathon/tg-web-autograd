27be00dccd0f63afe9aea414113aa7432415a503
SlavaKuzkinHackathon <jess.888@yandex.ru>
2023-01-14 16:55:23 +0700

    13.61

--- src/components/OtdelItem/OtdelItem.jsx
+++ src/components/OtdelItem/OtdelItem.jsx
@@ -34,6 +34,13 @@ const OtdelItem = ({ product, className, onAdd }) => {
             <a className={'phone'} href={"https://t.me/+79137009150"} target="_blank">
                 +79137009150
             </a>
+            <ul>
+                <li> Джозеф Доу </li>
+                <li> Джо </li>
+                <li> The Example Company </li>
+                <li> 604-555-1234 </li>
+                <li><a href="tel: +79137009150" > tel: +79137009150 </a></li>
+            </ul>
         </div>
     );
 };
